a5fde57be826b37b77534456293523e7b9fd921e
Johan Terblanche <48163155+Johan-Terblanche@users.noreply.github.com>
2020-10-15 21:08:20 -0500

    Add files via upload
    Testing upload functionality, currently using HARD CODED PATH

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using GoogleDriveRestAPI_v3.Models;
+using System;
+using System.Windows.Forms;
+
+namespace TimeTracker.View
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Console.WriteLine("Running");
+            GoogleDriveFilesRepository.FileUpload("Output2020_09_03.csv");
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+
+            
+            //Application.Run(new Form1());
+        }
+    }
+}
